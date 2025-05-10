004d7d655313ca9874968c05448abb88bbf85732
prostosandr <amramabramovic@yandex.ru>
2025-05-10 18:29:06 +0700

    Revert "Reapply "Попытка1""
    This reverts commit 092a16339b18462b29139c4c05799a6727bdc5bb.

--- 2DPlatformerBattle/Assets/Scripts/Health.cs
+++ 2DPlatformerBattle/Assets/Scripts/Health.cs
@@ -7,10 +7,6 @@ public class Health : MonoBehaviour
     [SerializeField] private int _minHealth;
     [SerializeField] private int _maxHealth;
 
-    public int CurrentHealth => _health;
-    public int MinHealth => _minHealth;
-    public int MaxHealth => _maxHealth;
-
     public event Action Destroyed;
 
     public void TakeHeal(int value)
@@ -25,7 +21,7 @@ public class Health : MonoBehaviour
     {
         _health -= damage;
 
-        if (_health <= _minHealth)
+        if (_health < _minHealth)
         {
             _health = _minHealth;
             Destroyed?.Invoke();
